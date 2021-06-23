5e42456c43a2e82ea61f973dd9ad54e629e363b7
Anton Riedel <anton.riedel@tum.de>
2021-06-23 18:08:55 +0200

    begin writing Resubmit.sh

---
+++ Resubmit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# File              : Resubmit.sh
+# Author            : Anton Riedel <anton.riedel@tum.de>
+# Date              : 23.06.2021
+# Last Modified Date: 23.06.2021
+# Last Modified By  : Anton Riedel <anton.riedel@tum.de>
+
+# search for jobs in grid that failed and resubmit them
+
+
