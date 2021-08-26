e78b7941faa5e4a5eee2127721e5c0585ed91596
ariedel-cern <ariedel@cern.ch>
2021-08-26 10:29:38 +0000

    Feat: add script for waiting for timeouts interactively

---
+++ GridTimeout.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# File              : GridTimeout.sh
+# Author            : Anton Riedel <anton.riedel@tum.de>
+# Date              : 26.08.2021
+# Last Modified Date: 26.08.2021
+# Last Modified By  : Anton Riedel <anton.riedel@tum.de>
+
+# use this script for waiting for timeout in GridUtilityScripts
+
+time=$(($1 - 1))
+while [ $time -ge 0 ]; do
+    echo -ne "Waiting: $time\033[0K\r"
+    sleep 1
+    ((time--))
+done
+echo
+
+exit 0
