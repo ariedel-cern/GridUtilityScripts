e05cc402a61eee66b0bdf3ce6394255d1ba752a3
ariedel-cern <ariedel@cern.ch>
2021-08-26 14:47:42 +0000

    Fix: add counters to keep track of progress

--- KillAllJobs.sh
+++ KillAllJobs.sh
@@ -2,33 +2,42 @@
 # File              : KillAllJobs.sh
 # Author            : Anton Riedel <anton.riedel@tum.de>
 # Date              : 05.08.2021
-# Last Modified Date: 08.08.2021
+# Last Modified Date: 26.08.2021
 # Last Modified By  : Anton Riedel <anton.riedel@tum.de>
 
 #clean up and kill all running jobs on the Grid
 
 # loop variables
 Subjobs=""
+MasterjobCounter=0
+SubjobCounter=0
 
 # get all masterjobs not in Done state
 Masterjobs="$(alien_ps -M | awk '$4!="D" {print $2}')"
-NMasterjobs="$(wc -w <<<$Masterjobs)"
+NumberOfMasterJobs="$(wc -w <<<$Masterjobs)"
 
-[ $NMasterjobs -eq 0 ] && echo "All Jobs are done. Get out!" && exit 0
+[ $NumberOfMasterJobs -eq 0 ] && echo "All Masterjobs are done. Get out!" && exit 0
 
-echo "$NMasterjobs are not DONE. Let's kill them all!"
+echo "$NumberOfMasterJobs Masterjobs are not DONE. Let's kill them all!"
 
 for Masterjob in $Masterjobs; do
-	echo "Lets get rid of $Masterjob"
-	# get all subjobs not in Done state
-	Subjobs="$(alien_ps -m $Masterjob | awk '$4!="D" {print $2}')"
-	echo "To be safe, we will kill its $(wc -w <<<$SubJobs) subjobs explicitly"
-	for Subjob in $Subjobs; do
-		echo "Kill subjob $Subjob"
-		alien.py kill $Subjob
-	done
-	echo "Kill the masterjob itself"
-	alien.py masterjob $Masterjob kill
+    echo "Lets get rid of Masterjob $Masterjob"
+    # get all subjobs not in Done state
+    Subjobs="$(alien_ps -m $Masterjob | awk '$4!="D" {print $2}')"
+    NumberOfSubjobs="$(wc -w $ <<<$Subjobs)"
+    echo "To be safe, we will kill its $(wc -w <<<$SubJobs) Subjobs explicitly"
+    SubjobCounter=0
+    for Subjob in $Subjobs; do
+        echo "Kill subjob $Subjob"
+        alien.py kill $Subjob
+        ((SubjobCounter++))
+        echo "Killed ${SubjobCounter}/${NumberOfSubjobs}"
+    done
+
+    echo "Kill the Masterjob $Masterjob itself"
+    alien.py masterjob $Masterjob kill
+    ((MasterjobCounter++))
+    echo "Killed ${MasterjobCounter}/${NumberOfMasterJobs}"
 done
 
 exit 0
--- KillFailedJobs.sh
+++ KillFailedJobs.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-# File              : KillAllFailedJobs.sh
+# File              : KillFailedJobs.sh
 # Author            : Anton Riedel <anton.riedel@tum.de>
 # Date              : 05.08.2021
-# Last Modified Date: 06.08.2021
+# Last Modified Date: 26.08.2021
 # Last Modified By  : Anton Riedel <anton.riedel@tum.de>
 
-# get all masterjobs not in Done state
+# get all jobs not in Done state
 Jobs="$(alien_ps -E | awk '{print $2}')"
 NJobs="$(wc -w <<<$Jobs)"
 
 [ $NJobs -eq 0 ] && echo "No Jobs in error state...." && exit 0
 
 for Job in $Jobs; do
-        echo "Kill $Job"
-        alien.py kill $Job
+    echo "Kill $Job"
+    alien.py kill $Job
 done
 
 exit 0
