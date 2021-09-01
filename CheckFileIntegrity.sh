90022df0039179fb197e9f1bf8cbd7911bdf6bdb
ariedel-cern <ariedel@cern.ch>
2021-09-01 16:48:48 +0200

    Fix: update CheckFileIntegrity to use new config file

--- CheckFileIntegrity.sh
+++ CheckFileIntegrity.sh
@@ -2,44 +2,32 @@
 # File              : CheckFileIntegrity.sh
 # Author            : Anton Riedel <anton.riedel@tum.de>
 # Date              : 17.06.2021
-# Last Modified Date: 30.06.2021
+# Last Modified Date: 01.09.2021
 # Last Modified By  : Anton Riedel <anton.riedel@tum.de>
 
-# check integrity of all .root files in a given directory and subdirectories
+# check integrity of all .root files in the local output directory
 
 # check integrity
 Check_Integrity() {
 
-	# return 1 if a check is not passed
-	file $File || return 1
-	rootls $File || return 1
+    # return 1 if a check is not passed
+    file $File || return 1
+    rootls $File || return 1
 
-	# return 0 if all checks are passed
-	return 0
+    # ADD MORE CHECKS
+
+    # return 0 if all checks are passed
+    return 0
 }
 
-# parse arguments
-# expect $1 to be path to configuration file CopyFromGrid.config
-ConfigFile=${1:-CopyFromGrid.config}
-if [ ! -f $ConfigFile ]; then
-	echo "No local config file, fallback to global default"
-	ConfigFile=$HOME/config/CopyFromGrid.config
-fi
+# source config file
+[ ! -f GridConfig.sh ] && echo "No config file!!!" && exit 1
+source GridConfig.sh
 
-# get variables from config file
-[ ! -f $ConfigFile ] && echo "No config file" && exit 1
-SearchPath="$(awk -F'=' '/SearchPath/{print $2}' $ConfigFile)"
-[ -z $SearchPath ] && echo "No directory in config file" && exit 1
-SearchPath=$(basename $SearchPath)
-[ ! -d $SearchPath ] && echo "No directory to search through" && exit 1
-GlobalLog="$(awk -F'=' '/GlobalLog/{print $2}' $ConfigFile)"
-[ -z $GlobalLog ] && echo "No global log file in config file" && exit 1
-echo $GlobalLog
-[ ! -f $GlobalLog ] && echo "No global log file found" && exit 1
-
-Files=$(find $SearchPath -type f -name "*.root")
+# declare variables
+Files=$(find $GridOutputDirRel -type f -name "*.root")
 NumberOfFiles=$(wc -l <<<$Files)
-echo "Checking .root $NumberOfFiles files in $(realpath $SearchPath)"
+echo "Checking $NumberOfFiles root files in $GridOutputDirRel"
 
 # count number of subshells
 CounterSubshells=0
@@ -49,33 +37,32 @@ CounterFiles=0
 # loop over all files and check file integrity
 while read File; do
 
-	# do not start too many jobs
-	if [ $CounterSubshells -ge $(nproc) ]; then
-		wait
-		CounterSubshells=0
-		echo "$CounterFiles/$NumberOfFiles files checked"
-	fi
+    # do not start too many jobs
+    if [ $CounterSubshells -ge $(nproc) ]; then
+        wait
+        CounterSubshells=0
+        echo "$CounterFiles/$NumberOfFiles files checked"
+    fi
 
-	# test file in subshell
-	(
-		echo "Testing $File"
-		if ! Check_Integrity $File &>/dev/null; then
-			echo "Fail... remove $File"
-			rm $File
-			sed -i -e "/${File//\//\\/}/d" $GlobalLog
-		else
-			echo "Pass... keep $File"
-		fi
-	) &
+    # test file in subshell
+    (
+        echo "Testing $File"
+        if ! Check_Integrity $File &>/dev/null; then
+            echo "Fail... remove $File"
+            rm $File
+            sed -i -e "/${File//\//\\/}/d" $GlobalLog
+        else
+            echo "Pass... keep $File"
+        fi
+    ) &
 
-	((CounterSubshells++))
-	((CounterFiles++))
+    ((CounterSubshells++))
+    ((CounterFiles++))
 
 done <<<$Files
 
 # wait for unfinished jobs
 wait
 echo "$CounterFiles/$NumberOfFiles files checked"
-echo "DONE"
 
 exit 0
