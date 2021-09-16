c2e9f4487dc7d986e381be6699a0027fa54a53ef
ariedel-cern <ariedel@cern.ch>
2021-09-16 19:16:33 +0200

    Fix: source GridConfig.sh too often

--- Resubmit_Loop.sh
+++ Resubmit_Loop.sh
@@ -2,20 +2,22 @@
 # File              : Resubmit_Loop.sh
 # Author            : Anton Riedel <anton.riedel@tum.de>
 # Date              : 07.09.2021
-# Last Modified Date: 08.09.2021
+# Last Modified Date: 15.09.2021
 # Last Modified By  : Anton Riedel <anton.riedel@tum.de>
 
 # resubmit jobs in an endless loop
+
+# source config file
+[ ! -f GridConfig.sh ] && echo "No config file!!!" && exit 1
+source GridConfig.sh
 
 trap 'echo && echo "Stopping the loop" && Flag=1' SIGINT
 
 Flag="0"
 
-while [ $Flag -eq "0" ];
-    do
-        source GridConfig.sh
-        Resubmit.sh
-        GridTimeout.sh $TIMEOUT
+while [ $Flag -eq "0" ]; do
+    Resubmit.sh
+    GridTimeout.sh $TIMEOUT
 done
 
 exit 0
