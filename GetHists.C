515fac612d6e90eb35fde9be894e05ed9da588b9
ariedel-cern <anton.riedel@cern.ch>
2021-10-14 12:21:05 +0200

    Feat: put InterateList into ist own macro

--- GetHists.C
+++ GetHists.C
@@ -2,49 +2,39 @@
  * File              : GetHists.C
  * Author            : Anton Riedel <anton.riedel@tum.de>
  * Date              : 15.09.2021
- * Last Modified Date: 16.09.2021
+ * Last Modified Date: 14.10.2021
  * Last Modified By  : Anton Riedel <anton.riedel@tum.de>
  */
 
-#include <boost/algorithm/string.hpp>
-
-void *IterateList(TList *list, std::string searchString, std::string prefix) {
-
-  TObject *hist;
-  std::string s;
-  for (auto key : *list) {
-    if (key->IsFolder()) {
-      IterateList(dynamic_cast<TList *>(key), searchString, prefix);
-    } else {
-      s = std::string(key->GetName());
-      if (boost::contains(s, searchString)) {
-        s = prefix + s;
-        std::cout << "Found:" << s << std::endl;
-        hist = key->Clone(s.c_str());
-        hist->Write();
-      }
-    }
-  }
-  return nullptr;
-}
+#include "InterateList.H"
 
 Int_t GetHists(const char *DataFile, const char *OutputFileName,
                const char *Search) {
 
   TFile *dataFile = new TFile(DataFile, "READ");
+
   // open output directory
   TDirectoryFile *tdirFile = dynamic_cast<TDirectoryFile *>(
       dataFile->Get(std::getenv("OUTPUT_TDIRECTORY_FILE")));
 
   TFile *outputFile = new TFile(OutputFileName, "UPDATE");
-  TList *list;
+  TList *searchList;
+  TList *resultList = new TList();
+  resultList->SetOwner(kTRUE);
   std::string searchString(Search);
   std::string prefix;
+
   for (auto KeyTask : *(tdirFile->GetListOfKeys())) {
-    list = dynamic_cast<TList *>(tdirFile->Get(KeyTask->GetName()));
+
+    searchList = dynamic_cast<TList *>(tdirFile->Get(KeyTask->GetName()));
     prefix = std::string(KeyTask->GetName());
     prefix += "_";
-    IterateList(list, searchString, prefix);
+    IterateList(searchList, resultList, searchString, prefix);
+
+    for (auto obj : *resultList) {
+      obj->Write();
+    }
+    resultList->Clear();
   }
 
   outputFile->Close();
