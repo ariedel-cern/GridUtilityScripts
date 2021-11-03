303935d805e182a9f37642cf0f9571bfe790fd51
ariedel-cern <anton.riedel@cern.ch>
2021-11-03 14:45:14 +0100

    Feat: add Macro for creating subsampling for bootstrap

--- Bootstrap.C
+++ Bootstrap.C
@@ -2,21 +2,24 @@
  * File              : Bootstrap.C
  * Author            : Anton Riedel <anton.riedel@tum.de>
  * Date              : 27.10.2021
- * Last Modified Date: 02.11.2021
+ * Last Modified Date: 03.11.2021
  * Last Modified By  : Anton Riedel <anton.riedel@tum.de>
  */
 
-R__ADD_INCLUDE_PATH($ALICE_ROOT)
-R__ADD_INCLUDE_PATH($ALICE_PHYSICS)
 #include "GridHelperMacros.H"
 
 Int_t Bootstrap(const char *FileSubSamplesName, const char *observable) {
 
+  // open file holding path to files, grouped into subsamples
+  //   the subsamples are divided by a new line
   std::ifstream FileSubSamples(FileSubSamplesName);
   std::string FileName;
 
+  // fill the path to the files into a vector of vector of strings
   std::vector<std::vector<std::string>> SubSampleFileNames;
   std::vector<std::string> FileNames;
+  std::vector<Double_t> centrality;
+  Double_t Lcen, Ucen;
 
   while (getline(FileSubSamples, FileName)) {
 
@@ -27,38 +30,33 @@ Int_t Bootstrap(const char *FileSubSamplesName, const char *observable) {
       FileNames.push_back(FileName);
     }
   }
-  SubSampleFileNames.push_back(FileNames);
+  // catch last subsample, there is no new line at the end of the file
+  // SubSampleFileNames.push_back(FileNames);
 
-  // TProfile *profile =
-  //     new TProfile("profile", "profile", SubSampleFileNames.size() + 1, 0,
-  //                  SubSampleFileNames.size() + 1);
-
-  Int_t test = 0;
-
+  // open the first one to figure out how many tasks there are
+  // we need to perform bootstrap on a task by task, read centrality class
   TFile *dataFile = new TFile(SubSampleFileNames.at(0).at(0).c_str(), "READ");
   TDirectoryFile *tdirFile = dynamic_cast<TDirectoryFile *>(
       dataFile->Get(std::getenv("OUTPUT_TDIRECTORY_FILE")));
-  Int_t NumberOfTasks = tdirFile->GetListOfKeys()->GetSize();
 
+  // book one TProfile for each task
+  // the first bin holds the overall average, the other bins the averages of the
+  // subsamples
   TList *profileList = new TList();
-
   for (auto KeyTask : *(tdirFile->GetListOfKeys())) {
     TList *list = dynamic_cast<TList *>(tdirFile->Get(KeyTask->GetName()));
     profileList->Add(new TProfile(
         Form("Bootstrap_%s_%s", KeyTask->GetName(), observable), observable,
         SubSampleFileNames.size() + 1, 0, SubSampleFileNames.size() + 1));
   }
-  // dataFile->Close();
 
-  Int_t index;
-
+  // start looping over all files/task and compute the averages
+  Int_t index = 0;
   for (std::size_t i = 0; i < SubSampleFileNames.size(); i++) {
 
     for (std::size_t j = 0; j < SubSampleFileNames.at(i).size(); j++) {
 
       dataFile = new TFile(SubSampleFileNames.at(i).at(j).c_str(), "READ");
-
-      // open output directory
       TDirectoryFile *tdirFile = dynamic_cast<TDirectoryFile *>(
           dataFile->Get(std::getenv("OUTPUT_TDIRECTORY_FILE")));
 
@@ -73,13 +71,51 @@ Int_t Bootstrap(const char *FileSubSamplesName, const char *observable) {
             ->Fill(i + 1.5, hist->GetBinContent(1));
 
         index++;
+
+        // keep here till we fix analysis task
+        // if (i == 0 && j == 0) {
+        //   Lcen = dynamic_cast<TH1 *>(
+        //              IterateList(list, std::string("EventCutValues")))
+        //              ->GetBinContent(11);
+        //   Ucen = dynamic_cast<TH1 *>(
+        //              IterateList(list, std::string("EventCutValues")))
+        //              ->GetBinContent(12);
+        //   std::cout << Lcen << " - " << Ucen << std::endl;
+        //   centrality.push_back((Ucen - Lcen) / 50.);
+        // }
       }
       dataFile->Close();
     }
   }
 
-  TFile *out = new TFile("out.root", "RECREATE");
-  profileList->Write("bootstrap.root");
+  Double_t var = 0;
+  TGraphErrors *g = new TGraphErrors();
+  g->SetName(observable);
+  g->SetTitle(observable);
+  g->GetXaxis()->SetTitle("centrality percentile");
+
+  // keep here till we fix analysis task
+  centrality = {2.5, 7.5, 15, 25, 35, 45, 55, 65, 75};
+  index = 0;
+  for (auto *p : *profileList) {
+
+    TH1 *t = dynamic_cast<TH1 *>(p);
+
+    var = 0;
+    for (int i = 2; i < t->GetNbinsX(); i++) {
+      var += std::pow(t->GetBinContent(i) - t->GetBinContent(1), 2);
+    }
+    var = std::sqrt(var / ((t->GetNbinsX() - 1) * (t->GetNbinsX() - 2)));
+
+    g->SetPoint(index, centrality.at(index), t->GetBinContent(1));
+    g->SetPointError(index, 0, var);
+
+    index++;
+  }
+
+  TFile *out = new TFile(std::getenv("LOCAL_OUTPUT_BOOTSTRAP_FILE"), "UPDATE");
+  // profileList->Write();
+  g->Write();
   out->Close();
 
   return 0;
--- Bootstrap.sh
+++ Bootstrap.sh
@@ -2,7 +2,7 @@
 # File              : Bootstrap.sh
 # Author            : Anton Riedel <anton.riedel@tum.de>
 # Date              : 27.10.2021
-# Last Modified Date: 02.11.2021
+# Last Modified Date: 03.11.2021
 # Last Modified By  : Anton Riedel <anton.riedel@tum.de>
 
 source GridConfig.sh
@@ -16,16 +16,30 @@ else
     exit 2
 fi
 
+if [ -f "$GRID_UTILITY_SCRIPTS/Subsamples.C" ]; then
+    SubsamplesMacro="$(realpath $GRID_UTILITY_SCRIPTS/Subsamples.C)"
+else
+    echo "No macro found for merging..."
+    echo 'Did you set $GRID_UTILITY_SCRIPTS properly?'
+    exit 2
+fi
+
 # devide runs into chunks with approximately the same number of events
 # use this placeholder for testing purposes for now
-SubSamples="SubSamples.txt"
+Subsamples="Subsamples.txt"
+
+if [ ! -f $LOCAL_SUBSAMPLES_FILE ]; then
+    Files="ReTerminated.txt"
+    find $GRID_OUTPUT_DIR_REL -type f -name "*ReTerminated.root" >$Files
+    aliroot -q -l -b $SubsamplesMacro\(\"$Files\",\"$LOCAL_SUBSAMPLES_FILE\"\)
+fi
 
 # clean up
 rm $LOCAL_OUTPUT_BOOTSTRAP_FILE 2>/dev/null
 
-for Bootstrap in $LOCAL_OUTPUT_BOOTSTRAP;do
+for Bootstrap in $LOCAL_OUTPUT_BOOTSTRAP; do
     echo "Bootstrap $Bootstrap"
-    aliroot -q -l -b $BootstrapMacro\(\"$SubSamples\",\"$Bootstrap\"\)
+    aliroot -q -l -b $BootstrapMacro\(\"$LOCAL_SUBSAMPLES_FILE\",\"$Bootstrap\"\)
 done
 
 exit 0
