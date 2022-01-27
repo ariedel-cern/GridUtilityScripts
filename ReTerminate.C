34e089ece8a1c5060a74810866a887284bdb8771
Anton Riedel <anton.riedel@tum.de>
2022-01-27 10:18:06 +0100

    Feat: update Reterminate scritps to work with new config

--- ReTerminate.C
+++ ReTerminate.C
@@ -2,16 +2,22 @@
  * File              : ReTerminate.C
  * Author            : Anton Riedel <anton.riedel@tum.de>
  * Date              : 14.05.2021
- * Last Modified Date: 08.10.2021
+ * Last Modified Date: 27.01.2022
  * Last Modified By  : Anton Riedel <anton.riedel@tum.de>
  */
 
 #include <boost/algorithm/string.hpp>
+#include <nlohmann/json.hpp>
 
 Int_t ReTerminate(const char *mergedFileName) {
 
+  std::fstream ConfigFile("config.json");
+  nlohmann::json Jconfig = nlohmann::json::parse(ConfigFile);
+
   // open merged file
   TFile *mergedFile = new TFile(mergedFileName, "READ");
+
+  // mergedFile->ls();
 
   // create new file so we do not overwrite the original
   std::string reterminatedFileName(mergedFileName);
@@ -22,11 +28,11 @@ Int_t ReTerminate(const char *mergedFileName) {
   AliAnalysisTaskAR *Task = new AliAnalysisTaskAR("ReTerminate");
 
   // get TDirectoryFile holding all outputs
-  TDirectoryFile *tdirFile = dynamic_cast<TDirectoryFile *>(
-      mergedFile->Get(std::getenv("OUTPUT_TDIRECTORY_FILE")));
+  TDirectoryFile *tdirFile = dynamic_cast<TDirectoryFile *>(mergedFile->Get(
+      Jconfig["task"]["OutputTDirectory"].get<std::string>().c_str()));
 
   /* create new TDirectoryFile to hold the updated outputs, give it the same
-   * name and tile as the old one, cloning not working? */
+   * name and title as the old one, cloning not working? */
   TDirectoryFile *newTdirFile =
       new TDirectoryFile(tdirFile->GetName(), tdirFile->GetTitle());
 
@@ -38,6 +44,8 @@ Int_t ReTerminate(const char *mergedFileName) {
   for (auto KeyTask : *(tdirFile->GetListOfKeys())) {
     // get the output list of a task
     TaskList = dynamic_cast<TList *>(tdirFile->Get(KeyTask->GetName()));
+    std::cout << "Working on Task: " << KeyTask->GetName() << " ... ";
+    // TaskList->ls();
     // initialize the task with the list
     Task->GetPointers(TaskList);
     // RETERMINATE
@@ -45,6 +53,7 @@ Int_t ReTerminate(const char *mergedFileName) {
     // write the reterminated list back to the new TDirectoryFile
     TaskList->Write(TaskList->GetName(),
                     TObject::kSingleKey + TObject::kOverwrite);
+    std::cout << "Done" << std::endl;
   }
 
   // write all reterminated objects back to file
--- ReTerminate.sh
+++ ReTerminate.sh
@@ -2,27 +2,18 @@
 # File              : ReTerminate.sh
 # Author            : Anton Riedel <anton.riedel@tum.de>
 # Date              : 31.08.2021
-# Last Modified Date: 03.12.2021
+# Last Modified Date: 27.01.2022
 # Last Modified By  : Anton Riedel <anton.riedel@tum.de>
 
 # reterminate merged .root files run by run
 
-# source config file
-[ ! -f GridConfig.sh ] && echo "No config file!!!" && exit 1
-source GridConfig.sh
+[ ! -f config.json ] && echo "No config file!!!" && exit 1
 
-# get macro we are wrapping around
-if [ -f "$GRID_UTILITY_SCRIPTS/ReTerminate.C" ]; then
-	ReTerminateMacro="$(realpath $GRID_UTILITY_SCRIPTS/ReTerminate.C)"
-else
-	echo "No macro found for merging..."
-	echo 'Did you set $GRID_UTILITY_SCRIPTS properly?'
-	exit 2
-fi
+OutputDir="$(jq -r '.task.GridOutputDir' config.json)"
 
-echo "Reterminate merged files in $GRID_OUTPUT_DIR_REL"
-echo "Using Macro $ReTerminateMacro"
+echo "Reterminate merged files in $OutputFile"
 
-find $GRID_OUTPUT_DIR_REL -type f -name "*Merged.root" | parallel --bar --progress "aliroot -b -l -q $ReTerminateMacro'(\"{}\")'"
+find $OutputDir -type f -name "*Merged.root" | parallel --bar --progress "aliroot -b -l -q $GRID_UTILITY_SCRIPTS/ReTerminate.C'(\"{}\")'"
+# find $OutputDir -type f -name "*Merged.root" -exec aliroot -b -l -q $GRID_UTILITY_SCRIPTS/ReTerminate.C\(\"{}\"\) \;
 
 exit 0
