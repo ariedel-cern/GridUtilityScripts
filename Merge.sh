841a7f92e7dbbd6c6a0933225867aeeff96dfbf1
Anton Riedel <anton.riedel@tum.de>
2022-02-03 09:58:52 +0100

    Feat: make Mergeing part of the automatic steering

--- Merge.sh
+++ Merge.sh
@@ -2,33 +2,71 @@
 # File              : Merge.sh
 # Author            : Anton Riedel <anton.riedel@tum.de>
 # Date              : 24.03.2021
-# Last Modified Date: 17.01.2022
+# Last Modified Date: 03.02.2022
 # Last Modified By  : Anton Riedel <anton.riedel@tum.de>
 
 # merge .root files run by run
 
 [ ! -f config.json ] && echo "No config file!!!" && exit 1
+LockFile="$(jq -r '.LockFile' config.json)"
+StatusFile="$(jq -r '.StatusFile' config.json)"
+[ ! -f $StatusFile ] && echo "No $StatusFile file!!!" && exit 1
 
 echo "Merging $(jq -r '.task.GridOutputFile' config.json) run by run in $(jq -r '.task.GridOutputDir' config.json)"
 
 MergedFile=""
 OutputFile="$(jq -r '.task.GridOutputFile' config.json)"
+Runs="$(jq -r '.Runs[]' config.json)"
 
-while read Run; do
-	echo "Start merging run $(basename $Run) in $(dirname $Run)"
+Data=""
+Status=""
+FilesCopied=""
+FilesChecked=""
+FilesMerged=""
+
+
+for Run in $Runs; do
+
+	{
+		flock 100
+		Data="$(jq -r --arg Run "$Run" '.[$Run]' $StatusFile)"
+	} 100>$LockFile
+
+	Status="$(jq -r '.Status' <<<$Data)"
+    FilesMerged="$(jq -r '.Merged' <<<$Data)"
+	FilesCopied="$(jq -r '.FilesCopied' <<<$Data)"
+	FilesChecked="$(jq -r '.FilesChecked' <<<$Data)"
+
+    [ "${Status:=RUNNING}" == "RUNNING" ] && continue
+    [ "${FilesMerged:=0}" -ge 1 ] && continue
+    [ "$((${FilesCopied:=0} - 3))" -ge "$FilesChecked" ] && contiune
+
+    RunDir="$(jq -r '.task.GridOutputDir' config.json)/${Run}"
 
 	# go into subdirectory
-	pushd $Run
+	pushd $RunDir
+
+	echo "Start merging run $Run in $PWD"
 
 	# construct filename for merged file
 	MergedFile="$(basename $Run)_Merged.root"
+    FilesToMerge="$(find . -type f -name "$OutputFile")"
 
 	# merge files using hadd in parallel!!!
-	hadd -f -k -j $(nproc) $MergedFile $(find . -type f -name "$OutputFile")
+	hadd -f -k -j $(nproc) $MergedFile <<<$FilesToMerge
+
+    FilesMerged="$(wc -l <<<$FilesToMerge)"
+
+    find . -type f -name "$OutputFile" -delete
 
 	# go back
 	popd
 
-done < <(find "$(jq -r '.task.GridOutputDir' config.json)" -maxdepth 1 -mindepth 1 -type d)
+    {
+            flock 100
+            jq --arg Run "$Run" --arg FilesMerged "${FilesMerged:=0}" 'setpath([$Run,"Merged"];$FilesMerged)' $StatusFile | sponge $StatusFile
+    } 100>$LockFile
+
+done
 
 exit 0
