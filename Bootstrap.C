feadd90eafb20a6662d4bebc4822ebd5ce454ef8
Anton Riedel <anton.riedel@tum.de>
2022-02-15 09:45:34 +0100

    Feat: begin reimplementing boostrap

--- Bootstrap.C
+++ Bootstrap.C
@@ -2,121 +2,147 @@
  * File              : Bootstrap.C
  * Author            : Anton Riedel <anton.riedel@tum.de>
  * Date              : 27.10.2021
- * Last Modified Date: 03.11.2021
+ * Last Modified Date: 15.02.2022
  * Last Modified By  : Anton Riedel <anton.riedel@tum.de>
  */
 
 #include "GridHelperMacros.H"
+#include <fstream>
+#include <nlohmann/json.hpp>
+#include <vector>
 
-Int_t Bootstrap(const char *FileSubSamplesName, const char *observable) {
+Int_t Bootstrap(const char *FileSubSamplesName) {
 
-  // open file holding path to files, grouped into subsamples
-  //   the subsamples are divided by a new line
-  std::ifstream FileSubSamples(FileSubSamplesName);
-  std::string FileName;
+  // load config file
+  std::fstream ConfigFile("config.json");
+  nlohmann::json Jconfig = nlohmann::json::parse(ConfigFile);
+
+  // load file containig the subsamples
+  std::fstream FileSubSamples(FileSubSamplesName);
+  nlohmann::json JSubSamples = nlohmann::json::parse(FileSubSamples);
 
   // fill the path to the files into a vector of vector of strings
   std::vector<std::vector<std::string>> SubSampleFileNames;
-  std::vector<std::string> FileNames;
-  std::vector<Double_t> centrality;
-  Double_t Lcen, Ucen;
-
-  while (getline(FileSubSamples, FileName)) {
-
-    if (FileName.empty()) {
-      SubSampleFileNames.push_back(FileNames);
-      FileNames.clear();
-    } else {
-      FileNames.push_back(FileName);
-    }
-  }
-  // catch last subsample, there is no new line at the end of the file
-  // SubSampleFileNames.push_back(FileNames);
-
-  // open the first one to figure out how many tasks there are
-  // we need to perform bootstrap on a task by task, read centrality class
-  TFile *dataFile = new TFile(SubSampleFileNames.at(0).at(0).c_str(), "READ");
-  TDirectoryFile *tdirFile = dynamic_cast<TDirectoryFile *>(
-      dataFile->Get(std::getenv("OUTPUT_TDIRECTORY_FILE")));
-
-  // book one TProfile for each task
-  // the first bin holds the overall average, the other bins the averages of the
-  // subsamples
-  TList *profileList = new TList();
-  for (auto KeyTask : *(tdirFile->GetListOfKeys())) {
-    TList *list = dynamic_cast<TList *>(tdirFile->Get(KeyTask->GetName()));
-    profileList->Add(new TProfile(
-        Form("Bootstrap_%s_%s", KeyTask->GetName(), observable), observable,
-        SubSampleFileNames.size() + 1, 0, SubSampleFileNames.size() + 1));
+  for (nlohmann::json::iterator it = JSubSamples.begin();
+       it != JSubSamples.end(); ++it) {
+    SubSampleFileNames.push_back(
+        JSubSamples[it.key()].get<std::vector<std::string>>());
   }
 
-  // start looping over all files/task and compute the averages
-  Int_t index = 0;
-  for (std::size_t i = 0; i < SubSampleFileNames.size(); i++) {
+  // create vector of names all tasks
+  std::vector<std::string> Tasks;
+  TFile *DummyFile = new TFile(SubSampleFileNames.at(0).at(0).c_str(), "READ");
+  TDirectoryFile *tdirFile = dynamic_cast<TDirectoryFile *>(DummyFile->Get(
+      Jconfig["task"]["OutputTDirectory"].get<std::string>().c_str()));
 
-    for (std::size_t j = 0; j < SubSampleFileNames.at(i).size(); j++) {
+  TList *TaskList = tdirFile->GetListOfKeys();
+  for (auto key : *TaskList) {
+    Tasks.push_back(std::string(key->GetName()));
+  }
 
-      dataFile = new TFile(SubSampleFileNames.at(i).at(j).c_str(), "READ");
-      TDirectoryFile *tdirFile = dynamic_cast<TDirectoryFile *>(
-          dataFile->Get(std::getenv("OUTPUT_TDIRECTORY_FILE")));
+  // create a vector of all names of observabels
+  std::vector<std::string> SCs;
+  AliAnalysisTaskAR *Task = new AliAnalysisTaskAR("Dummy");
 
-      index = 0;
-      for (auto KeyTask : *(tdirFile->GetListOfKeys())) {
-        TList *list = dynamic_cast<TList *>(tdirFile->Get(KeyTask->GetName()));
-        TH1 *hist =
-            dynamic_cast<TH1 *>(IterateList(list, std::string(observable)));
-        dynamic_cast<TProfile *>(profileList->At(index))
-            ->Fill(0.5, hist->GetBinContent(1));
-        dynamic_cast<TProfile *>(profileList->At(index))
-            ->Fill(i + 1.5, hist->GetBinContent(1));
+  Task->GetPointers(dynamic_cast<TList *>(
+      tdirFile->Get(tdirFile->GetListOfKeys()->First()->GetName())));
 
-        index++;
+  TList *SCList =
+      dynamic_cast<TList *>(Task->GetFinalResultSymmetricCumulantsList());
 
-        // keep here till we fix analysis task
-        // if (i == 0 && j == 0) {
-        //   Lcen = dynamic_cast<TH1 *>(
-        //              IterateList(list, std::string("EventCutValues")))
-        //              ->GetBinContent(11);
-        //   Ucen = dynamic_cast<TH1 *>(
-        //              IterateList(list, std::string("EventCutValues")))
-        //              ->GetBinContent(12);
-        //   std::cout << Lcen << " - " << Ucen << std::endl;
-        //   centrality.push_back((Ucen - Lcen) / 50.);
-        // }
-      }
-      dataFile->Close();
+  for (auto key : *SCList) {
+    SCs.push_back(std::string(key->GetName()));
+  }
+
+  // close dummy file and start boostrapping
+  DummyFile->Close();
+
+  for (auto Task : Tasks) {
+    for (auto SC : SCs) {
+      std::cout << SC << " in " << Task << std::endl;
     }
   }
 
-  Double_t var = 0;
-  TGraphErrors *g = new TGraphErrors();
-  g->SetName(observable);
-  g->SetTitle(observable);
-  g->GetXaxis()->SetTitle("centrality percentile");
-
-  // keep here till we fix analysis task
-  centrality = {2.5, 7.5, 15, 25, 35, 45, 55, 65, 75};
-  index = 0;
-  for (auto *p : *profileList) {
-
-    TH1 *t = dynamic_cast<TH1 *>(p);
-
-    var = 0;
-    for (int i = 2; i < t->GetNbinsX(); i++) {
-      var += std::pow(t->GetBinContent(i) - t->GetBinContent(1), 2);
-    }
-    var = std::sqrt(var / ((t->GetNbinsX() - 1) * (t->GetNbinsX() - 2)));
-
-    g->SetPoint(index, centrality.at(index), t->GetBinContent(1));
-    g->SetPointError(index, 0, var);
-
-    index++;
-  }
-
-  TFile *out = new TFile(std::getenv("LOCAL_OUTPUT_BOOTSTRAP_FILE"), "UPDATE");
-  // profileList->Write();
-  g->Write();
-  out->Close();
+  // // book one TProfile for each task
+  // // the first bin holds the overall average, the other bins the averages of
+  // the
+  // // subsamples
+  // TList *profileList = new TList();
+  // for (auto KeyTask : *(tdirFile->GetListOfKeys())) {
+  //   TList *list = dynamic_cast<TList *>(tdirFile->Get(KeyTask->GetName()));
+  //   profileList->Add(new TProfile(
+  //       Form("Bootstrap_%s_%s", KeyTask->GetName(), observable), observable,
+  //       SubSampleFileNames.size() + 1, 0, SubSampleFileNames.size() + 1));
+  // }
+  //
+  // // start looping over all files/task and compute the averages
+  // Int_t index = 0;
+  // for (std::size_t i = 0; i < SubSampleFileNames.size(); i++) {
+  //
+  //   for (std::size_t j = 0; j < SubSampleFileNames.at(i).size(); j++) {
+  //
+  //     dataFile = new TFile(SubSampleFileNames.at(i).at(j).c_str(), "READ");
+  //     TDirectoryFile *tdirFile = dynamic_cast<TDirectoryFile *>(
+  //         dataFile->Get(std::getenv("OUTPUT_TDIRECTORY_FILE")));
+  //
+  //     index = 0;
+  //     for (auto KeyTask : *(tdirFile->GetListOfKeys())) {
+  //       TList *list = dynamic_cast<TList
+  //       *>(tdirFile->Get(KeyTask->GetName())); TH1 *hist =
+  //           dynamic_cast<TH1 *>(IterateList(list, std::string(observable)));
+  //       dynamic_cast<TProfile *>(profileList->At(index))
+  //           ->Fill(0.5, hist->GetBinContent(1));
+  //       dynamic_cast<TProfile *>(profileList->At(index))
+  //           ->Fill(i + 1.5, hist->GetBinContent(1));
+  //
+  //       index++;
+  //
+  //       // keep here till we fix analysis task
+  //       // if (i == 0 && j == 0) {
+  //       //   Lcen = dynamic_cast<TH1 *>(
+  //       //              IterateList(list, std::string("EventCutValues")))
+  //       //              ->GetBinContent(11);
+  //       //   Ucen = dynamic_cast<TH1 *>(
+  //       //              IterateList(list, std::string("EventCutValues")))
+  //       //              ->GetBinContent(12);
+  //       //   std::cout << Lcen << " - " << Ucen << std::endl;
+  //       //   centrality.push_back((Ucen - Lcen) / 50.);
+  //       // }
+  //     }
+  //     dataFile->Close();
+  //   }
+  // }
+  //
+  // Double_t var = 0;
+  // TGraphErrors *g = new TGraphErrors();
+  // g->SetName(observable);
+  // g->SetTitle(observable);
+  // g->GetXaxis()->SetTitle("centrality percentile");
+  //
+  // // keep here till we fix analysis task
+  // centrality = {2.5, 7.5, 15, 25, 35, 45, 55, 65, 75};
+  // index = 0;
+  // for (auto *p : *profileList) {
+  //
+  //   TH1 *t = dynamic_cast<TH1 *>(p);
+  //
+  //   var = 0;
+  //   for (int i = 2; i < t->GetNbinsX(); i++) {
+  //     var += std::pow(t->GetBinContent(i) - t->GetBinContent(1), 2);
+  //   }
+  //   var = std::sqrt(var / ((t->GetNbinsX() - 1) * (t->GetNbinsX() - 2)));
+  //
+  //   g->SetPoint(index, centrality.at(index), t->GetBinContent(1));
+  //   g->SetPointError(index, 0, var);
+  //
+  //   index++;
+  // }
+  //
+  // TFile *out = new TFile(std::getenv("LOCAL_OUTPUT_BOOTSTRAP_FILE"),
+  // "UPDATE");
+  // // profileList->Write();
+  // g->Write();
+  // out->Close();
 
   return 0;
 }
--- Bootstrap.sh
+++ Bootstrap.sh
@@ -2,46 +2,14 @@
 # File              : Bootstrap.sh
 # Author            : Anton Riedel <anton.riedel@tum.de>
 # Date              : 27.10.2021
-# Last Modified Date: 03.12.2021
+# Last Modified Date: 14.02.2022
 # Last Modified By  : Anton Riedel <anton.riedel@tum.de>
 
-# source config file
-[ ! -f GridConfig.sh ] && echo "No config file!!!" && exit 1
-source GridConfig.sh
+# bootstrap all FinalResultProfile
 
-# get macro we are wrapping around
-if [ -f "$GRID_UTILITY_SCRIPTS/Bootstrap.C" ]; then
-	BootstrapMacro="$(realpath $GRID_UTILITY_SCRIPTS/Bootstrap.C)"
-else
-	echo "No macro found for merging..."
-	echo 'Did you set $GRID_UTILITY_SCRIPTS properly?'
-	exit 2
-fi
+[ ! -f config.json ] && echo "No config file!!!" && exit 1
 
-if [ -f "$GRID_UTILITY_SCRIPTS/Subsamples.C" ]; then
-	SubsamplesMacro="$(realpath $GRID_UTILITY_SCRIPTS/Subsamples.C)"
-else
-	echo "No macro found for merging..."
-	echo 'Did you set $GRID_UTILITY_SCRIPTS properly?'
-	exit 2
-fi
-
-# devide runs into chunks with approximately the same number of events
-# use this placeholder for testing purposes for now
-Subsamples="Subsamples.txt"
-
-if [ ! -f $LOCAL_SUBSAMPLES_FILE ]; then
-	Files="ReTerminated.txt"
-	find $GRID_OUTPUT_DIR_REL -type f -name "*ReTerminated.root" >$Files
-	aliroot -q -l -b $SubsamplesMacro\(\"$Files\",\"$LOCAL_SUBSAMPLES_FILE\"\)
-fi
-
-# clean up
-rm $LOCAL_OUTPUT_BOOTSTRAP_FILE 2>/dev/null
-
-for Bootstrap in $LOCAL_OUTPUT_BOOTSTRAP; do
-	echo "Bootstrap $Bootstrap"
-	aliroot -q -l -b $BootstrapMacro\(\"$LOCAL_SUBSAMPLES_FILE\",\"$Bootstrap\"\)
-done
+echo "Performing Bootstrap"
+aliroot -q -l -b $GRID_UTILITY_SCRIPTS/Bootstrap.C\(\"$GRID_UTILITY_SCRIPTS/SUBSAMPLES.json\"\)
 
 exit 0
